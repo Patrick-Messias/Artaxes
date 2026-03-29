a775038c03bebecb713eec7f860825a017cab191
Patrick <patrick.98mb@gmail.com>
2026-03-28 20:55:49 -0400

    minor work done in portfolio

--- Backend/core/Portfolio.py
+++ Backend/core/Portfolio.py
@@ -5,6 +5,7 @@ from typing import Optional
 from PortfolioSystemManager import PortfolioSystemManager
 from PortfolioMoneyManager import PortfolioMoneyManager
 from Storage import Storage
+from Asset import Asset
 import polars as pl, uuid
 
 """
@@ -30,6 +31,8 @@ class PortfolioParams():
     datetime_timeline: set=field(default_factory=set)
     portfolio_returns: dict=None
 
+    use_portfolio_asset_data: bool=True
+
 class Portfolio(): 
     def __init__(self, portfolio_params: PortfolioParams):
         self.name = portfolio_params.name
@@ -41,35 +44,22 @@ class Portfolio():
         self.datetime_timeline = portfolio_params.datetime_timeline
         self.portfolio_returns = portfolio_params.portfolio_returns
 
-    def _get_all_portfolio_data(self) -> dict:
-        return self.portfolio_data if self.portfolio_data else {}
+        self.use_portfolio_asset_data = portfolio_params.use_portfolio_asset_data
     
     def _run(self):
         # Data Init - Uses already uploaded data or loads from drive with Storage.py
         print("     > Populating Portfolio Data from Database")
-        self._load_saved_data()
+        self._load_selected_saved_returns_data()
 
         # Maps all unique datetimes to use as simulator timeline
         print("     > Mapping all unique datetimes from Portfolio Data")
         self._map_all_unique_datetimes()
 
-
-
-
         # Runs Portfolio Simulation
-        #print("     > Executing Portfolio Simulation")
-        #self._simulation()
+        print("     > Executing Portfolio Simulation")
+        self._simulation()
             
-
-
         return True
-
-
-
-
-
-
-
 
     def _simulation(self):
 
@@ -86,23 +76,21 @@ class Portfolio():
     def _map_all_unique_datetimes(self):
         unique_dts = set()
 
-        # From Portfolio Data
-        for *_, a_name, a_obj in self._iter_portfolio_data():        
-            if "pnl_matrix" in a_obj:
-                unique_dts.update(a_obj["pnl_matrix"]["ts"])
-            elif "wf" in a_obj:
-                unique_dts.update(a_obj["wf"]["datetime"])
-            else: 
-                print(f"< [Error] No PnL or Walkforward data found for Asset: {a_name}")
+        if self.use_portfolio_asset_data: # From Portfolio Assets
+            unique_dts.update(self._load_all_op_asset_datetimes())
+
+        else: # From Portfolio Operation results data
+            for *_, a_name, a_obj in self._iter_portfolio_data():        
+                if "pnl_matrix" in a_obj:
+                    unique_dts.update(a_obj["pnl_matrix"]["ts"])
+                elif "wf" in a_obj:
+                    unique_dts.update(a_obj["wf"]["datetime"])
+                else: 
+                    print(f"< [Error] No PnL or Walkforward data found for Asset: {a_name}")
 
         # From System/Money Manager Assets
 
 
-        # -> Resolver problema minutos wf
-        # -> IMPORTANTE -> AO INVÉS DE PEGAR DATETIMES DOS WF/PNL, USAR DOS ASSETS DOS MODELOS, JÁ QUE PROVAVELMENTE VAI USAR NO SYSTEM/MONEY
-        # -> Como vai ficar a estrutura de System/Money? ter uma lista de indicators e assets que vão ser usados?
-        
-        # money_manager_equilizer = {"frequency": 0.5 trades per day, "avg_win", "avg_loss"}
 
         self.datetime_timeline = sorted(list(unique_dts))
 
@@ -110,8 +98,23 @@ class Portfolio():
         for dt in self.datetime_timeline[:5] + self.datetime_timeline[-5:]:
             print(dt.strftime("%d-%m-%y %H:%M:%S"))
         return True
+    
+    def _load_all_op_asset_datetimes(self):
+        unique_assets_dts = set()
 
-    def _load_saved_data(self):
+        # NOTE After updating Asset storage, update below
+
+        for *_, a_name in self._iter_portfolio_data(): 
+            # Loads assets from Operation map
+            assets_class = Asset(date_start=None, date_end=None)
+            asset_df = assets_class.data_get(a_name) # pl.DataFrame with column "ts"
+
+            #Adds to unique
+            unique_assets_dts.update(asset_df["datetime"])
+
+        return unique_assets_dts
+
+    def _load_selected_saved_returns_data(self): # Loads all data from selected map
         storage = Storage(base_path=self.data_storage_base_path)
 
         for op_name, _, m_name, _, s_name, _, a_name, a_obj in self._iter_portfolio_data():
@@ -143,7 +146,17 @@ if __name__ == "__main__":
     portfolio = Portfolio(PortfolioParams(name="Portfolio_Test", portfolio_data=portfolio_data))
     portfolio._run()
 
+    """
+    
 
+    # XXX -> Resolver problema minutos wf
+    # -> IMPORTANTE -> AO INVÉS DE PEGAR DATETIMES DOS WF/PNL, USAR DOS ASSETS DOS MODELOS, JÁ QUE PROVAVELMENTE VAI USAR NO SYSTEM/MONEY
+    # -> Como vai ficar a estrutura de System/Money? ter uma lista de indicators e assets que vão ser usados?
+
+    # money_manager_equilizer = {"frequency": 0.5 trades per day, "avg_win", "avg_loss"}    
+    
+    
+    """
 
 
 
